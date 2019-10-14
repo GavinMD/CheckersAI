5b5e8136cb035f8d99b32e553bd6ab73f16888a8
gandrusi <gavinmd66@gmail.com>
2019-10-13 21:09:36 -0400

    integrated more moves

--- Checkers1/src/Game.java
+++ Checkers1/src/Game.java
@@ -41,10 +41,10 @@ public class Game extends JFrame {
 
 		add(b);
 		setTitle("Checkers");
-		setSize(808, 830);
+		setSize(808, 650);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		setResizable(false);
+		setResizable(true);
 		setVisible(true);
 
 		play();
@@ -341,37 +341,37 @@ public class Game extends JFrame {
 	}
 
 	// class move creates a move object for ease of passing objects
-	public class Move {
-		int from; // starting position of the checker
-		int to; // end position of the checker
-		int inter = -1;
+// 	public class Move {
+// 		int from; // starting position of the checker
+// 		int to; // end position of the checker
+// 		int inter = -1;
 
-		public Move(int from, int to) {
-			this.from = from;
-			this.to = to;
-		}
+// 		public Move(int from, int to) {
+// 			this.from = from;
+// 			this.to = to;
+// 		}
 
-		public Move(int from, int to, int inter) {
-			this.from = from;
-			this.to = to;
-			this.inter = inter;
-		}
+// 		public Move(int from, int to, int inter) {
+// 			this.from = from;
+// 			this.to = to;
+// 			this.inter = inter;
+// 		}
 
-		public boolean isJump() { // checks if the move is a jump
-			return to - from == 9 || to - from == 7 || from - to == 9 || from - to == 7 || inter != -1;
-		}
+// 		public boolean isJump() { // checks if the move is a jump
+// 			return to - from == 9 || to - from == 7 || from - to == 9 || from - to == 7 || inter != -1;
+// 		}
 
-		// checks if the moves are equal
-		public boolean equals(Move other) {
-			return other.from == this.from && other.to == this.to;
-		}
+// 		// checks if the moves are equal
+// 		public boolean equals(Move other) {
+// 			return other.from == this.from && other.to == this.to;
+// 		}
 
-		public String toString() {
-			return "From: " + from + " To position: " + to;
-		}
+// 		public String toString() {
+// 			return "From: " + from + " To position: " + to;
+// 		}
 
-//		
-	}
+// //		
+// 	}
 
 	// technically the board loop
 	public void play() {
